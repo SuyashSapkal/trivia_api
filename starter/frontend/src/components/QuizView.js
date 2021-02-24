0ea01458029c5dd5b83b8ad6dfd432a95d753dfe
Suyash Sapkal <suyashsapkal21@gmail.com>
2021-02-24 17:37:15 +0530

    changes made in 'quizview in frontend'

--- starter/frontend/src/components/QuizView.js
+++ starter/frontend/src/components/QuizView.js
@@ -132,6 +132,9 @@ class QuizView extends Component {
 
   evaluateAnswer = () => {
     const formatGuess = this.state.guess.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase()
+    if(formatGuess==''){
+      return false
+    }
     const answerArray = this.state.currentQuestion.answer.toLowerCase().split(' ');
     return answerArray.includes(formatGuess)
   }
